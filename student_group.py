53d558d24dc60cc06140a16dc272c5782f66f54f
Brian Mukeswe <39747968+mukeswebrian@users.noreply.github.com>
2019-09-30 14:41:42 -0700

    added author information

--- student_group.py
+++ student_group.py
@@ -1,3 +1,10 @@
+'''
+Author: Brian Mukeswe
+Date: September 30, 2019
+Contact: mukeswebrian@yahoo.com
+
+This class defines a student group that can be assigned to a session
+'''
 class StudentGroup():
     
     def __init__(self, group_id):
